39846f7d5340360bb72d7b52006858a41725c624
Detrimonious <zasamar@gmail.com>
2021-04-17 19:01:46 -0700

    Complete hover grids but script is in HTML and not JS sheet

---
+++ README.md
@@ -0,0 +1,5 @@
+JavaScript DOM project -- part of The Odin Project: Foundations --
+to create grid of square divs that function like an Etch-a-Sketch
+by hovering over and activating the divs.
+
+Uses DOM to make grid divs
--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const containerDiv = document.querySelector(#container);
-const gridDiv = document.createElement('div');
+const containerDiv = document.querySelector('#container');
+containerDiv.style.cssText = 'grid-template-rows: repeat(16, minmax(50px, 1fr)); grid-template-columns: repeat(16, minmax(50px, 1fr))';
 
-gridDiv.style.display = 'grid';
+function createGrids(){
+    for (let i=0; i<16; i++){
+        const gridDiv = document.createElement('div');
+        containerDiv.appendChild(gridDiv);
+    }
+}
